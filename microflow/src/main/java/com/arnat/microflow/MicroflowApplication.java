ad709827d8c38a8cdc0a0ad2514eca7cb300ff2d
Ariel Rivero <jrivero@hipotecario.com.ar>
2025-06-01 20:44:54 -0300

    Agrego core app

---
+++ microflow/src/main/java/com/arnat/microflow/MicroflowApplication.java
@@ -0,0 +1,13 @@
+package com.arnat.microflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroflowApplication.class, args);
+	}
+
+}
---
+++ microflow/src/test/java/com/arnat/microflow/MicroflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnat.microflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
