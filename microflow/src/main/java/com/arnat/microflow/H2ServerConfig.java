6f508953494d6a962c3760c82d3a3da169d6fdfd
Ariel Rivero <jrivero@hipotecario.com.ar>
2025-06-01 21:03:50 -0300

    Agrego conexión h2

---
+++ microflow/src/main/java/com/arnat/microflow/H2ServerConfig.java
@@ -0,0 +1,28 @@
+package com.arnat.microflow;
+
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+
+public class H2ServerConfig {
+
+
+    private Server tcpServer;
+
+    @PostConstruct
+    public void startTcpServer() throws SQLException {
+        this.tcpServer = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
+        System.out.println("H2 TCP server started at port 9092");
+    }
+
+    @PreDestroy
+    public void stopTcpServer() {
+        if (tcpServer != null) {
+            tcpServer.stop();
+        }
+    }
+}
